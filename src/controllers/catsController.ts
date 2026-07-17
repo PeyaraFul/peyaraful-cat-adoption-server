e6d28df8462463a3c73d036d90a93819b4e52799
PeyaraFul <arakash022@gmail.com>
2026-07-17 23:30:35 +0600

    feat: add cats controller skeleton with function exports
    - Created controllers/catsController.ts
    - Defined getAllCats, getCatById, createCat, updateCat, deleteCat exports

---
+++ src/controllers/catsController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import { getDB } from '../config/db';
+import { ObjectId } from 'mongodb';
+
+export const getAllCats = async (req: Request, res: Response) => {};
+export const getCatById = async (req: Request, res: Response) => {};
+export const createCat = async (req: Request, res: Response) => {};
+export const updateCat = async (req: Request, res: Response) => {};
+export const deleteCat = async (req: Request, res: Response) => {};
