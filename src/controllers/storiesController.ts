3cde5bfe5089a4211c03de1487b2dec81c9438c6
PeyaraFul <arakash022@gmail.com>
2026-07-18 08:35:24 +0600

    feat: add stories controller skeleton with function exports

---
+++ src/controllers/storiesController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import { getDB } from '../config/db';
+import { ObjectId } from 'mongodb';
+
+export const getAllStories = async (req: Request, res: Response) => {};
+export const getTopStories = async (req: Request, res: Response) => {};
+export const createStory = async (req: Request, res: Response) => {};
+export const deleteStory = async (req: Request, res: Response) => {};
